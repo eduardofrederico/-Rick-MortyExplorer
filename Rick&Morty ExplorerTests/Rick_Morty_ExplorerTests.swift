6bebe382968728775bc27c5bd2a845c68a46890f
Eduardo Frederico <eduardo.frederico@icloud.com>
2025-04-09 22:06:27 -0300

    Initial Commit

---
+++ Rick&Morty ExplorerTests/Rick_Morty_ExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_Morty_ExplorerTests.swift
+//  Rick&Morty ExplorerTests
+//
+//  Created by Eduardo Frederico on 09/04/25.
+//
+
+import Testing
+@testable import Rick_Morty_Explorer
+
+struct Rick_Morty_ExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Rick&Morty ExplorerUITests/Rick_Morty_ExplorerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rick_Morty_ExplorerUITestsLaunchTests.swift
+//  Rick&Morty ExplorerUITests
+//
+//  Created by Eduardo Frederico on 09/04/25.
+//
+
+import XCTest
+
+final class Rick_Morty_ExplorerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
