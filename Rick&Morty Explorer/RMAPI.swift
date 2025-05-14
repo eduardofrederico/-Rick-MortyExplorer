c8bcfa1f54fb556190b9fcacc8efe34f8d177a24
Eduardo Frederico <eduardo.frederico@icloud.com>
2025-05-13 21:17:44 -0300

    Ajustando a View Controller principal do projeto.

---
+++ Rick&Morty Explorer/RMAPI.swift
@@ -0,0 +1,8 @@
+//
+//  RMAPI.swift
+//  Rick&Morty Explorer
+//
+//  Created by Eduardo Frederico Sotero da Costa              on 07/05/25.
+//
+
+import Foundation
---
+++ Rick&Morty Explorer/RMInfo.swift
@@ -0,0 +1,8 @@
+//
+//  RMInfo.swift
+//  Rick&Morty Explorer
+//
+//  Created by Eduardo Frederico Sotero da Costa              on 07/05/25.
+//
+
+import Foundation
